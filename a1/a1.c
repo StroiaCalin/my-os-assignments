e93e638c5fe5749b3170b5056ee587488ca9cdb1
Calin Stroia <stroiacalin@yahoo.com>
2023-04-09 03:00:41 +0300

    extract

--- a1/a1.c
+++ a1/a1.c
@@ -7,6 +7,201 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+
+void extract(char* path, char* section, char* line)
+{	
+
+
+	int ok=0;
+	char magic[5] = "TO2Y";
+	
+	int size_aux=0;
+	int offset_aux=0;
+	
+	int a=atoi(section);/// conversie la int
+	int b=atoi(line);
+
+    magic[4] = '\0';
+    int fd;
+    short header_size=0;
+    int version=0;
+    short number_of_section=0;
+    fd = open(path, O_RDONLY);
+	char * str = calloc(strlen(magic)+1,sizeof(char));
+	read(fd,str,4);
+	read(fd,&header_size,2);
+	//printf("%d\n",header_size);
+	read(fd,&version,4);
+
+	read(fd,&number_of_section,1);
+	int * array=calloc(number_of_section,sizeof(int));
+	int aux=0;
+	
+	
+	//// bucla pentru a introduce memType in array
+
+	for(int i=0;i<number_of_section;i++)
+	{
+	int aux;
+	lseek(fd,13,SEEK_CUR);
+	read(fd,&aux,4);
+	array[i]=aux;  ////// array pentru sect_type
+	lseek(fd,8,SEEK_CUR);
+	}
+
+	for(int i=0;i<number_of_section;i++)
+	{
+	if(array[i]!=37 && array[i]!=94 && array[i]!=87 && array[i]!=14)
+	{
+	printf("ERROR\ninvalid file");
+	ok++;
+	aux++;
+	}
+	}
+	int linii=0;
+	if(aux == 0)
+	{
+	lseek(fd,11,SEEK_SET);/// se intoarce unde trebe sa citeasca
+	for(int i=0;i<number_of_section;i++)
+	{
+	char * SECTION_HEADERS = calloc(13,sizeof(char));
+	read(fd,SECTION_HEADERS,13);
+	int SECT_TYPE=0;
+	
+	read(fd,&SECT_TYPE,4);
+
+	//printf("SECT_TYPE%d\n",SECT_TYPE);
+	
+	
+	int SECT_OFFSET=0;
+	
+	read(fd,&SECT_OFFSET,4);
+	//printf("SECT_OFFSET%d\n",SECT_OFFSET);
+	int SECT_SIZE=0;
+	read(fd,&SECT_SIZE,4);
+	if(a>number_of_section)
+	{
+	printf("ERROR\ninvalid section");
+	ok++;
+	break;
+	}
+	if(i==a-1)
+	{
+	
+	int pas=lseek(fd,0,SEEK_CUR);
+	offset_aux=SECT_OFFSET;
+	size_aux=SECT_SIZE;
+	
+	lseek(fd,offset_aux,SEEK_CUR);
+	
+	char aux;
+	
+	for(int i=0;i<size_aux;i++)
+	{
+	read(fd,&aux,1);
+	if(aux=='\n')
+	{
+	linii++;
+	}
+	
+	}
+	lseek(fd,pas,SEEK_SET);
+	if(b>linii)
+	{
+	printf("ERROR\ninvalid line");
+	ok++;
+	break;
+	
+	}
+	}
+	if(strcmp(magic,str)!= 0)
+	{
+	printf("ERROR\ninvalid file");
+	ok++;
+	break;
+	}
+	if(version<108 || version> 140)
+	{
+	printf("ERROR\ninvalid file");
+	ok++;
+	break;
+	}
+	if(number_of_section<5 || number_of_section>13)
+	{
+	printf("ERROR\ninvalid file");
+	ok++;
+	break;
+	}
+	}
+	}
+	
+	char calinutz;
+	//int iteratii=1;
+	//printf("SUCCESS\n");
+	//printf("BBBBBBBBBBBBBBBBBBBB\n%d\nBBBBBBBBBBBBBBBBBBB",b);
+	//	printf("LLLL\n%d\n",linii);
+	lseek(fd,offset_aux,SEEK_SET);
+	char * iesire = calloc(size_aux+1,sizeof(char));
+	for(int i=0;i<size_aux;i++)
+	{
+	read(fd,&calinutz,1);
+	iesire[i]=calinutz;
+	}
+	iesire[size_aux]='\0';
+	char *token;
+    int linie = b;
+    int i = linii;
+    
+    
+   //printf("%s",iesire);
+   printf("SUCCESS\n");
+ 
+ 	//printf("\nBBBBBBBBBBBBB%d\n",b);
+    
+    token = strtok(iesire, "\n");
+    
+    while (token != NULL) {
+        if (i == linie-2) {
+            printf("%s\n", token);
+            break;
+        }
+        token = strtok(NULL, "\n");
+        i--;
+    }
+    
+	
+	
+	/*
+	int iteratii=1;
+	for(int i=0;i<size_aux;i++)
+	{
+	read(fd,&calinutz,1);
+  //printf("%c",calinutz);
+	if(calinutz=='\n')
+	{
+	iteratii++;
+	}
+	
+	if(iteratii == linii+b)
+	{
+	break;
+	}
+	}
+	char frumuselul;
+	while(frumuselul!='\n')
+	{
+	read(fd,&frumuselul,1);
+	printf("%c",frumuselul);
+	}
+	*/
+	free(array);
+	close(fd);
+}
+
+
+
+
+
 void cautareRec(const char *path)//////////////////////////// cautare recursiva
 {
     DIR *dir = NULL;
@@ -61,7 +256,7 @@ void print_permission(struct stat fileStat,char * perm,char *path,char * string)
     char* permissions = (char*) malloc(sizeof(char) * 10);
     memset(permissions, 0, 10);
    
-    permissions[0] = (fileStat.st_mode & S_IRUSR) ? 'r' : '-';
+    permissions[0] = (fileStat.st_mode & S_IRUSR) ? 'r' : '-';///////// stackoverflow " How to print ls -l in C"
     permissions[1] = (fileStat.st_mode & S_IWUSR) ? 'w' : '-';
     permissions[2] = (fileStat.st_mode & S_IXUSR) ? 'x' : '-';
     permissions[3] = (fileStat.st_mode & S_IRGRP) ? 'r' : '-';
@@ -238,6 +433,26 @@ printf("ERROR\nno arguments");
 	strncpy(conditii_perm,start1,end1-start1);
 	conditii_perm[end1-start1]='\0';
         cautare_perm(path,conditii_perm);
+        }
+        else if(argc == 5 && strcmp(argv[1],"extract")==0)
+        {
+        char* start = strchr(argv[2],'=')+1;// imi ia pathu
+	char* end = argv[2] + strlen(argv[2]);
+	char path[end-start];
+	strncpy(path,start,end-start);
+	path[end-start]='\0';
+        char* start1 = strchr(argv[3],'=')+1;// imi ia section
+	char* end1 = argv[3] + strlen(argv[3]);
+	char section[end1-start1];
+	strncpy(section,start1,end1-start1);
+	section[end1-start1]='\0';
+	char* start2 = strchr(argv[4],'=')+1;// imi ia line
+	char* end2 = argv[4] + strlen(argv[4]);
+	char line[end2-start2];
+	strncpy(line,start2,end2-start2);
+	line[end2-start2]='\0';
+	//printf("%s\n%s\n%s\n",path,section,line);
+	extract(path,section,line);	
         }
         else if( argc == 3 && strcmp(argv[1],"list") == 0) {   /////// aici nu am conditii
        	char* start = strchr(argv[2],'=')+1;// imi ia pathu
